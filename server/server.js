5b00fb15547cc85197455798a5aced3a4b3ed38d
benxoa <benxoax@gmail.com>
2023-12-17 15:43:56 +0500

    server changes

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require("dotenv").config({path: '.env'})
+require("dotenv").config({ path: '.env' });
 const port = process.env.PORT || 3000;
-require("./db/conn")
-const router = require('./router/router')
-const cookieParser = require('cookie-parser')
-const path = require('path')
-
-
+require("./db/conn");
+const router = require('./router/router');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
 app.use(express.json());
-const _dirname = path.dirname("")
-const buildpath = path.join(_dirname, "../client/dist")
-app.use(express.static(buildpath))
-app.use(cors({origin:"*", credentials: true}));
-app.use(cookieParser());
-app.use(router)
 
-app.listen(port, ()=> console.log("Connected to http://localhost:3000"))
+const _dirname = path.resolve();
+const buildpath = path.join(_dirname, "client", "dist");
+app.use(express.static(buildpath));
+
+app.use(cors({ origin: "*", credentials: true }));
+app.use(cookieParser());
+app.use(router);
+
+app.listen(port, () => console.log(`Connected to http://localhost:${port}`));
